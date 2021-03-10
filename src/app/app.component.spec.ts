deda07b1f113c918e47aa39ddccfa2582bb8c385
grantdb2000 <78166201+grantdb2000@users.noreply.github.com>
2021-03-10 03:47:59 -0500

    Push stuff

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'IHSAA-main'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('IHSAA-main');
+    expect(app.appName).toEqual('IHSAA-main');
   });
 
   it('should render title', () => {
